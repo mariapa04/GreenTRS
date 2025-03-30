e38bb4b789a071fa076b2b9bb88323d87ce2940b
mariapa04 <mariapaulaobandosolis@gmail.com>
2025-03-29 23:55:36 -0600

    TM3100

---
+++ src/main/java/cr/ac/ucr/greentrs/GreenTRS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.ucr.greentrs;
+
+/**
+ *
+ * @author Paula
+ */
+public class GreenTRS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
